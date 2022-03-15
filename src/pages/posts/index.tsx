7d8400017760983d021ee853c90e0ca10d21b171
ThiagoLp <thiagoluiz.ps@hotmail.com>
2022-03-15 16:41:12 -0300

    JAMStack aula - Listando post em tela

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,35 +1,73 @@
+import { GetStaticProps } from "next";
 import Head from "next/head";
+import { getPrismicClient } from "../../services/prismic";
+import Prismic from '@prismicio/client'
+import { RichText } from 'prismic-dom'
+
 import styles from './styles.module.scss'
 
+type Post = {
+  slug: string;
+  title: string;
+  excerpt: string;
+  updatedAt: string;
+}
 
-export default function Posts(){
+interface PostsProps {
+  posts: Post[]
+}
+
+export default function Posts({ posts }: PostsProps){
   return (
     <>
       <Head>
         <title>Posts | Ignews </title>  
-      </Head>    
+      </Head>   
+
       <main className={styles.container}>
         <div className={styles.posts}>
-          <a href="#" >
-            <time>15 de março de 2022</time>
-            <strong>JAMStack uma geléia de Javascript</strong>
-            <p> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aspernatur beatae qui necessitatibus minima fugit rem alias dolores iure molestias sed fuga placeat soluta impedit ab, et recusandae voluptatum perferendis corporis! </p>
-          </a>
-          <a href="#">
-            <time>15 de março de 2022</time>
-            <strong>JAMStack uma geléia de Javascript</strong>
-            <p> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aspernatur beatae qui necessitatibus minima fugit rem alias dolores iure molestias sed fuga placeat soluta impedit ab, et recusandae voluptatum perferendis corporis! </p>
-          </a>
-          <a href="#">
-            <time>15 de março de 2022</time>
-            <strong>JAMStack uma geléia de Javascript</strong>
-            <p> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aspernatur beatae qui necessitatibus minima fugit rem alias dolores iure molestias sed fuga placeat soluta impedit ab, et recusandae voluptatum perferendis corporis! </p>
-          </a>
+          {posts.map(post => (
+            <a key={post.slug} href="#" >
+              <time>{post.updatedAt}</time>
+              <strong>{post.title}</strong>
+              <p>{post.excerpt}</p>
+            </a>
+          ))}
         </div>
       </main>
     </>
-
-
-
   )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient()
+
+  const response = await prismic.query([
+    Prismic.predicates.at('document.type', 'posts')
+  ], {
+    fetch: ['posts.title', 'posts.content'],
+    pageSize: 100
+  })
+
+  const posts = response.results.map(post => {
+    return {
+      slug: post.uid,
+      title: RichText.asText(post.data.title),
+      excerpt: post.data.content.find(content => content.type === 'paragraph')?.text ?? '',
+      updatedAt: new Date(post.last_publication_date).toLocaleDateString(
+        'pt-BR',
+        {
+          day: '2-digit',
+          month: 'long',
+          year: 'numeric'
+        }
+      )
+    }
+  })
+
+  return {
+    props: {
+      posts
+    }
+  }
 }
