f55bc274b17b6285da55dacd75a9cf6180b4a95b
ThiagoLp <thiagoluiz.ps@hotmail.com>
2022-03-16 10:45:04 -0300

    JAMStack aula - Navegação do Menu

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,5 @@
+import Link from 'next/link'
+import { ActiveLink } from '../ActiveLink'
 import { SingInButton } from '../SingInButton'
 
 import styles from './styles.module.scss'
@@ -5,11 +7,15 @@ import styles from './styles.module.scss'
 export function Header(){
   return (
     <header className={styles.headerContainer}>
-      <div className={styles.headerContent}>
+      <div className={styles.headerContent}>  
         <img src="/images/logo.svg" alt="logo" />
         <nav>
-          <a href="/" className={styles.active}>Home</a>
-          <a href="/posts">Posts</a>
+          <ActiveLink href="/" activeCLass={styles.active}>
+            <a className={styles.active}>Home</a>
+          </ActiveLink>
+          <ActiveLink href="/posts" activeCLass={styles.active}>
+            <a>Posts</a>  
+          </ActiveLink>
         </nav>
         <SingInButton/>
       </div>
